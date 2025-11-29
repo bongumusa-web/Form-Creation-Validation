83dc6dcdcc729dcfcd7d47f3e4b4524e24fc33a3
Bongumusa Gumede <bonggums@gmail.com>
2025-11-29 13:51:52 +0200

    fetch-data.html, fetch-data.css, fetch-data.js

---
+++ fetch-data.js
@@ -0,0 +1,32 @@
+async function fetchUserData() {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+    const dataContainer = document.getElementById('api-data');
+
+    try{
+        const response = await fetch(apiUrl);
+        const users =await response.json(); // converting data to JSON format
+
+        dataContainer.innerHTML = ''; // Clear previous data
+
+        const userList = document.createElement('ul'); // creacting a list to display users
+        users.forEach(user => {
+            const li = document.createElement('li');
+            li.textContent = user.name;  // displaying user name
+            userList.appendChild(li);   // adding list item to the user list
+        });
+        dataContainer.appendChild(userList); // adding user list to the container
+
+  }
+    catch(error){
+        dataContainer.innerHTML = 'Failed to load data.'; // error handling
+        console.error('Error fetching data:', error);
+
+   }
+
+        
+    
+}
+
+
+//envoking 
+document.addEventListener('DOMContentLoaded', fetchUserData);
